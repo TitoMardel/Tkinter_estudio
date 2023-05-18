27dbb732d318bca9fd4a82ed3b9f713be1fb0236
TitoMardel <infoaver@gmail.com>
2023-05-18 19:12:33 -0300

    Estudio TKinter con Bootstrap

--- procesos.py
+++ procesos.py
@@ -1,24 +1,9 @@
-from pyModbusTCP.client import ModbusClient
+a=10
 
-#c = ModbusClient(host="127.0.0.1", auto_open=True, auto_close=False)
-try:
-    c = ModbusClient(host="127.0.0.1", auto_open=True, auto_close=False)
-except ValueError:
-    print("Error with host or port params")
+def sumar():
+    #a=1
+    print(a)
 
-print(c.unit_id)
-print(c.last_error_as_txt)
-
-if c.open():
-    print(c.last_error_as_txt)
-    print("pasa por if")
-    regs = c.read_holding_registers(0, 10)
-    if regs:
-        print(regs)
-    else:
-        print("read error")
-else:
-    print("no esta conectado")
-    print(c.last_error_as_txt)
+sumar()
 
 
